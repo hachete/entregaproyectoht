d50852af9ab17ea9a95a33e896858c1616803e8e
hachete <52215181+hachete@users.noreply.github.com>
2023-03-30 12:09:21 -0300

    Add files via upload

--- function.js
+++ function.js
@@ -4,3 +4,19 @@ document.getElementById("hora").innerHTML = d.toLocaleTimeString();
 }
 setInterval(time, 1000);
 
+
+
+
+
+const barras = document.querySelectorAll('.barra');
+
+window.addEventListener('scroll', () => {
+  const alturaVentana = window.innerHeight;
+  barras.forEach(barra => {
+    const posicionBarra = barra.getBoundingClientRect().top;
+    if (posicionBarra - alturaVentana <= 0) {
+      const anchoBarra = barra.getAttribute('data-ancho');
+      barra.style.width = anchoBarra;
+    }
+  });
+});
